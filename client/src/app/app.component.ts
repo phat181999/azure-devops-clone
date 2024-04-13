ba17a51f9c625160ec8b0ead334ff5e17015311f
phat181999 <phat181999@gmail.com>
2024-04-13 16:23:23 +0700

    implement SSO Keycloak

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { JSONEditor } from 'vanilla-jsoneditor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'client';
+  @ViewChild('jsoneditor', { static: true }) jsonEditorElementRef:
+    | ElementRef
+    | any;
+  constructor() {}
+  ngOnInit(): void {
+    const content = {
+      text: undefined,
+      json: {
+        name: 'John Doe',
+        age: 30,
+        email: 'john.doe@example.com',
+        address: {
+          street: '123 Main St',
+          city: 'Anytown',
+          country: 'USA',
+        },
+        is_active: true,
+        hobbies: ['reading', 'cooking', 'hiking'],
+        friends: [
+          {
+            name: 'Alice',
+            age: 28,
+            email: 'alice@example.com',
+          },
+          {
+            name: 'Bob',
+            age: 32,
+            email: 'bob@example.com',
+          },
+        ],
+      },
+    };
+
+    new JSONEditor({
+      target: this.jsonEditorElementRef.nativeElement,
+      props: {
+        content,
+
+        onChange: (
+          updatedContent,
+          previousContent,
+          { contentErrors, patchResult }
+        ) => {
+          console.log('onChange', {
+            updatedContent,
+            previousContent,
+            contentErrors,
+            patchResult,
+          });
+        },
+      },
+    });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+// Function to initialize Keycloak
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak
+      .init({
+        config: {
+          url: 'http://localhost:8080',
+          realm: 'azure-devops',
+          clientId: 'azure-devops-client',
+        },
+        initOptions: {
+          onLoad: 'login-required',
+          flow: 'standard',
+        },
+      })
+      .then(() => {
+        console.log('Keycloak initialized successfully');
+      })
+      .catch((error) => {
+        console.error('Error initializing Keycloak', error);
+      });
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    KeycloakAngularModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
